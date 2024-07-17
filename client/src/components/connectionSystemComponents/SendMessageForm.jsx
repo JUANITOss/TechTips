ef929dfd1d00f016b925b66693987980e5ba93fe
tomas <regueiratomas123@gmail.com>
2024-07-16 22:12:04 -0300

    vista telegram bot

--- client/src/components/connectionSystemComponents/SendMessageForm.jsx
+++ client/src/components/connectionSystemComponents/SendMessageForm.jsx
@@ -8,7 +8,8 @@ const SendMessageForm = () => {
   const [contactId, setContactId] = useState('');
   const [messageText, setMessageText] = useState('');
   const [statusMessage, setStatusMessage] = useState('');
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false); // Estado para el pop-up de añadir contacto
+  const [showTutorialModal, setShowTutorialModal] = useState(false); // Estado para el pop-up de tutorial
   const [newContactName, setNewContactName] = useState('');
   const [newContactId, setNewContactId] = useState('');
 
@@ -40,6 +41,10 @@ const SendMessageForm = () => {
 
   const toggleModal = () => {
     setShowModal(!showModal);
+  };
+
+  const toggleTutorialModal = () => {
+    setShowTutorialModal(!showTutorialModal);
   };
 
   const handleNewContactChange = (e) => {
@@ -78,7 +83,7 @@ const SendMessageForm = () => {
           <div className="center-container">
             <div className="message-form">
               <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center mb-6 md:mb-8 lg:mb-10 text-black">
-                Send a Message
+                Contactar Asistente 
               </h2>
               <form onSubmit={handleSubmit} className="form-content">
                 <div className="form-group">
@@ -134,33 +139,39 @@ const SendMessageForm = () => {
                         required
                       />
                     </div>
-                    <details>
-                      <summary>Tutorial</summary>
-                      <p className="disclaimer">
-                        Como agrego un asistente?
-                        <br />
-                        <br />
-                        <b>PASO 1: Obtener el Id del asistente por agregar</b>
-                        <br />
-                        Para obtener el Id, el asistente debe abrir la barra de búsqueda de Telegram, tipear userinfobot y copiar el id que le devuelva al campo de Contact Id al momento de agregar un nuevo asistente, el nombre puede ser personalizado.
-                        <br />
-                        <br />
-                        <b>PASO 2: Sincronizar al asistente con nuestro bot</b>
-                        <br />
-                        Para sincronizar al asistente con nuestro bot, el asistente debe abrir la siguiente url e iniciar el chat: http://t.me/techTipsMcPrisma_bot
-                        <br />
-                        <br />
-                        <b>PASO 3: Ya está! Ahora el usuario debe seleccionar a su asistente desde el selector y escribir su mensaje.</b>
-                      </p>
-                    </details>
-                    <div className="button-group">
-                      <button type="button" className="submit-button" onClick={handleCreateContact}>Create Contact</button>
-                      <button type="button" className="submit-button" onClick={toggleModal}>Cancel</button>
-                    </div>
+                    <button type="button" className="submit-button" onClick={handleCreateContact}>Create Contact</button>
+                    <button type="button" className="submit-button" onClick={toggleModal}>Cancel</button>
                   </form>
                 </div>
               </div>
             )}
+            <div className="popup">
+              <button className="popup-button" onClick={toggleTutorialModal}>Tutorial</button>
+              {showTutorialModal && (
+                <div className="modal">
+                  <div className="modal-content">
+                    <h2>Tutorial</h2>
+                    <p className="disclaimer">
+                      Como agrego un asistente?
+                      <br />
+                      <br />
+                      <b>PASO 1: Obtener el Id del asistente por agregar</b>
+                      <br />
+                      Para obtener el Id, el asistente debe abrir la barra de búsqueda de Telegram, tipear userinfobot y copiar el id que le devuelva al campo de Contact Id al momento de agregar un nuevo asistente, el nombre puede ser personalizado.
+                      <br />
+                      <br />
+                      <b>PASO 2: Sincronizar al asistente con nuestro bot</b>
+                      <br />
+                      Para sincronizar al asistente con nuestro bot, el asistente debe abrir la siguiente url e iniciar el chat: http://t.me/techTipsMcPrisma_bot
+                      <br />
+                      <br />
+                      <b>PASO 3: Ya está! Ahora el usuario debe seleccionar a su asistente desde el selector y escribir su mensaje.</b>
+                    </p>
+                    <button className="popup-close" onClick={toggleTutorialModal}>Cerrar</button>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </main>
